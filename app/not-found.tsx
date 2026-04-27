585da7b6db102631a1e3bbd126e666eac40cc7ae
Aryan Chauhan <aryan22chauhan07@gmail.com>
2026-04-27 18:49:03 +0530

    fix: not-found page error

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! This page doesn’t exist.</p>
+    </div>
+  );
+}
